1b630e567dbf5a8472fd391d32f564b124bf41c6
Takasaki Kazunari <kz-takasaki@kddi-agile.com>
2023-08-07 14:27:01 +0900

    2. 明白な実装

--- ts/money.test.ts
+++ ts/money.test.ts
@@ -1,6 +1,8 @@
 import { Dollar } from "./money";
 test("should return times", () => {
   const five = new Dollar(5);
-  five.times(2);
-  expect(five.amount).toEqual(10);
+  let product = five.times(2);
+  expect(product.amount).toEqual(10);
+  product = five.times(3);
+  expect(product.amount).toEqual(15);
 });
