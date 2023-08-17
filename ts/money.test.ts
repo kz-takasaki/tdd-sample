2900c476926734ed26611d340576a63639862663
Takasaki Kazunari <kz-takasaki@kddi-agile.com>
2023-08-17 10:49:27 +0900

    5. 原則をあえて破るとき

--- ts/money.test.ts
+++ ts/money.test.ts
@@ -1,4 +1,6 @@
 import Dollar from "./dollar";
+import Franc from "./franc";
+
 test("should return times", () => {
   const five = new Dollar(5);
   expect(five.times(2)).toEqual(new Dollar(10));
@@ -8,4 +10,10 @@ test("should return times", () => {
 test("equality", () => {
   expect(new Dollar(5).equals(new Dollar(5))).toEqual(true);
   expect(new Dollar(5).equals(new Dollar(6))).toEqual(false);
+});
+
+test("should return times franc", () => {
+  const five = new Franc(5);
+  expect(five.times(2)).toEqual(new Franc(10));
+  expect(five.times(3)).toEqual(new Franc(15));
 });
