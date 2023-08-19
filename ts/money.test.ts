a9c7d1c25e2c423b8a1b37c98b8fcf4a3045ef7d
Takasaki Kazunari <kz-takasaki@kddi-agile.com>
2023-08-19 17:23:46 +0900

    11. 不要になったら消す

--- ts/money.test.ts
+++ ts/money.test.ts
@@ -1,5 +1,4 @@
 import Money from "./money";
-import { Franc } from "./money";
 
 test("should return times", () => {
   const five: Money = Money.dollar(5);
@@ -10,23 +9,11 @@ test("should return times", () => {
 test("equality", () => {
   expect(Money.dollar(5).equals(Money.dollar(5))).toEqual(true);
   expect(Money.dollar(5).equals(Money.dollar(6))).toEqual(false);
-  expect(Money.franc(5).equals(Money.franc(5))).toEqual(true);
-  expect(Money.franc(5).equals(Money.franc(6))).toEqual(false);
   expect(Money.franc(5).equals(Money.dollar(5))).toEqual(false);
   expect(Money.dollar(5).equals(Money.franc(5))).toEqual(false);
-});
-
-test("should return times franc", () => {
-  const five: Money = Money.franc(5);
-  expect(five.times(2)).toEqual(Money.franc(10));
-  expect(five.times(3)).toEqual(Money.franc(15));
 });
 
 test("currency", () => {
   expect(Money.dollar(1).getCurrency()).toEqual("USD");
   expect(Money.franc(1).getCurrency()).toEqual("CHF");
-});
-
-test("different class equality", () => {
-  expect(new Money(10, "CHF").equals(new Franc(10, "CHF"))).toEqual(true);
 });
--- ts/money.ts
+++ ts/money.ts
@@ -18,21 +18,9 @@ export default class Money {
     );
   }
   static dollar(amount: number): Money {
-    return new Dollar(amount, "USD");
+    return new Money(amount, "USD");
   }
   static franc(amount: number): Money {
-    return new Franc(amount, "CHF");
-  }
-}
-
-class Dollar extends Money {
-  constructor(amount: number, currency: string) {
-    super(amount, currency);
-  }
-}
-
-export class Franc extends Money {
-  constructor(amount: number, currency: string) {
-    super(amount, currency);
+    return new Money(amount, "CHF");
   }
 }
