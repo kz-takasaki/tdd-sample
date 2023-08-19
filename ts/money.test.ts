074d7b6fcb4e9f9fa62b714719f156a7fe1fab8d
Takasaki Kazunari <kz-takasaki@kddi-agile.com>
2023-08-19 16:28:26 +0900

    8. 実装を隠す

--- ts/money.test.ts
+++ ts/money.test.ts
@@ -1,23 +1,22 @@
-import Dollar from "./dollar";
-import Franc from "./franc";
+import Money from "./money";
 
 test("should return times", () => {
-  const five = new Dollar(5);
-  expect(five.times(2)).toEqual(new Dollar(10));
-  expect(five.times(3)).toEqual(new Dollar(15));
+  const five: Money = Money.dollar(5);
+  expect(five.times(2)).toEqual(Money.dollar(10));
+  expect(five.times(3)).toEqual(Money.dollar(15));
 });
 
 test("equality", () => {
-  expect(new Dollar(5).equals(new Dollar(5))).toEqual(true);
-  expect(new Dollar(5).equals(new Dollar(6))).toEqual(false);
-  expect(new Franc(5).equals(new Franc(5))).toEqual(true);
-  expect(new Franc(5).equals(new Franc(6))).toEqual(false);
-  expect(new Franc(5).equals(new Dollar(5))).toEqual(false);
-  expect(new Dollar(5).equals(new Franc(5))).toEqual(false);
+  expect(Money.dollar(5).equals(Money.dollar(5))).toEqual(true);
+  expect(Money.dollar(5).equals(Money.dollar(6))).toEqual(false);
+  expect(Money.franc(5).equals(Money.franc(5))).toEqual(true);
+  expect(Money.franc(5).equals(Money.franc(6))).toEqual(false);
+  expect(Money.franc(5).equals(Money.dollar(5))).toEqual(false);
+  expect(Money.dollar(5).equals(Money.franc(5))).toEqual(false);
 });
 
 test("should return times franc", () => {
-  const five = new Franc(5);
-  expect(five.times(2)).toEqual(new Franc(10));
-  expect(five.times(3)).toEqual(new Franc(15));
+  const five: Money = Money.franc(5);
+  expect(five.times(2)).toEqual(Money.franc(10));
+  expect(five.times(3)).toEqual(Money.franc(15));
 });
--- ts/money.ts
+++ ts/money.ts
@@ -1,10 +1,45 @@
-export default class Money {
+export default abstract class Money {
   protected amount: number;
   constructor(amount: number) {
     this.amount = amount;
   }
+  abstract times(multiplier: number): Money;
   equals(object: Object): boolean {
     const money: Money = object as Money;
     return this.amount === money.amount;
+  }
+  static dollar(amount: number): Money {
+    return new Dollar(amount);
+  }
+  static franc(amount: number): Money {
+    return new Franc(amount);
+  }
+}
+
+class Dollar extends Money {
+  constructor(amount: number) {
+    super(amount);
+  }
+  times(multiplier: number): Money {
+    return new Dollar(this.amount * multiplier);
+  }
+  equals(object: Object): boolean {
+    return (
+      super.equals(object) && object.constructor.name === this.constructor.name
+    );
+  }
+}
+
+class Franc extends Money {
+  constructor(amount: number) {
+    super(amount);
+  }
+  times(multiplier: number): Money {
+    return new Franc(this.amount * multiplier);
+  }
+  equals(object: Object): boolean {
+    return (
+      super.equals(object) && object.constructor.name === this.constructor.name
+    );
   }
 }
